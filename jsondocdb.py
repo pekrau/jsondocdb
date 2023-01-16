4b79e96688ef6405cbe5843cf9f1b291c26f1bda
Per Kraulis <per.kraulis@gmail.com>
2023-01-16 16:06:49 +0100

    Working on indexes.

--- jsondocdb.py
+++ jsondocdb.py
@@ -5,9 +5,10 @@ Simple JSON document database with indexes; Python, Sqlite3 and JsonLogic.
 The Logic class was adapted from https://github.com/nadirizr/json-logic-py
 """
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 
 
+import functools
 import json
 import re
 import sqlite3
@@ -125,6 +126,7 @@ class Jsondocdb:
             cursor.execute("INSERT INTO docs (id, doc) VALUES (?, ?)", (id, doc))
         except sqlite3.IntegrityError:
             cursor.execute("UPDATE docs SET doc=? where id=?", (doc, id))
+        # Add to indexes.
 
     def __delitem__(self, id):
         """Delete the document with the given id from the database.
@@ -167,13 +169,6 @@ class Jsondocdb:
         "Are we within a transaction?"
         return self.cnx.in_transaction
 
-    def __del__(self):
-        """Close the database connection.
-
-        Raises InTransactionError if within a transaction.
-        """
-        self.close()
-
     def get(self, id, default=None):
         "Return the document with the given id. If not found, return the 'default'."
         try:
@@ -190,7 +185,7 @@ class Jsondocdb:
         return (row[0] for row in self.cnx.execute("SELECT doc FROM docs ORDER BY id"))
 
     def items(self):
-        "Return an iterator over all tuples (id, document)n in the database."
+        "Return an iterator over all tuples (id, document) in the database."
         return ((row[0], row[1]) for row in self.cnx.execute("SELECT id, doc FROM docs ORDER BY id"))
 
     def delete(self, id):
@@ -206,6 +201,22 @@ class Jsondocdb:
         if cursor.rowcount != 1:
             raise NoSuchDocumentError
         cursor.execute("DELETE FROM attachments WHERE docid=?", (id,))
+        # XXX Remove from indexes
+
+    def add_index(self, name, path, require=None):
+        """Add an index with the given name to the database.
+        All current documents will be indexed, so it might take a while.
+
+        path: The path in the data JSON document to index. If the path yields None,
+        the document is not included in the index.
+
+        require: An optional jsonLogic expression. Only documents satisfying
+        the condition are included in the index.
+        """
+        raise NotImplemented
+
+    def delete_index(self, name):
+        raise NotImplemented
 
     def get_indexes(self):
         "Return the list of names for the current indexes."
@@ -223,18 +234,217 @@ class Jsondocdb:
         "Return the number of attachments in the database."
         return self.cnx.execute("SELECT COUNT(*) FROM attachments").fetchone()[0]
 
-    def close(self):
-        """Close the database connection.
 
-        Raises InTransactionError if within a transaction.
+
+class Logic:
+    """Implementation of JsonLogic https://jsonlogic.com/
+
+    Code copied and adapted from https://github.com/nadirizr/json-logic-py
+    """
+
+    def __init__(self, expression):
+        self.expression = expression or {}
+
+
+    @staticmethod
+    def if_(*args):
+        """Implements the 'if' operator with support for multiple elseif-s."""
+        for i in range(0, len(args) - 1, 2):
+            if args[i]:
+                return args[i + 1]
+        if len(args) % 2:
+            return args[-1]
+        else:
+            return None
+
+
+    @staticmethod
+    def soft_equals(a, b):
+        """Implements the '==' operator, which does type JS-style coertion."""
+        if isinstance(a, str) or isinstance(b, str):
+            return str(a) == str(b)
+        if isinstance(a, bool) or isinstance(b, bool):
+            return bool(a) is bool(b)
+        return a == b
+
+
+    @staticmethod
+    def hard_equals(a, b):
+        """Implements the '===' operator."""
+        if type(a) != type(b):
+            return False
+        return a == b
+
+
+    @staticmethod
+    def less(a, b, *args):
+        """Implements the '<' operator with JS-style type coertion."""
+        types = set([type(a), type(b)])
+        if float in types or int in types:
+            try:
+                a, b = float(a), float(b)
+            except TypeError:
+                # NaN
+                return False
+        return a < b and (not args or less(b, *args))
+
+
+    @staticmethod
+    def less_or_equal(a, b, *args):
+        """Implements the '<=' operator with JS-style type coertion."""
+        return (
+            less(a, b) or soft_equals(a, b)
+        ) and (not args or less_or_equal(b, *args))
+
+
+    @staticmethod
+    def to_numeric(arg):
         """
-        if self.in_transaction:
-            raise InTransactionError("Cannot close while in a transaction.")
+        Converts a string either to int or to float.
+        This is important, because e.g. {"!==": [{"+": "0"}, 0.0]}
+        """
+        if isinstance(arg, str):
+            if '.' in arg:
+                return float(arg)
+            else:
+                return int(arg)
+        return arg
+
+    @staticmethod
+    def plus(*args):
+        """Sum converts either to ints or to floats."""
+        return sum(to_numeric(arg) for arg in args)
+
+
+    @staticmethod
+    def minus(*args):
+        """Also, converts either to ints or to floats."""
+        if len(args) == 1:
+            return -to_numeric(args[0])
+        return to_numeric(args[0]) - to_numeric(args[1])
+
+
+    @staticmethod
+    def merge(*args):
+        """Implements the 'merge' operator for merging lists."""
+        ret = []
+        for arg in args:
+            if isinstance(arg, list) or isinstance(arg, tuple):
+                ret += list(arg)
+            else:
+                ret.append(arg)
+        return ret
+
+
+    @staticmethod
+    def get_var(data, var_name, not_found=None):
+        """Gets variable value from data dictionary."""
         try:
-            self.cnx.close()
-            del self.cnx
-        except AttributeError:
-            pass
+            for key in str(var_name).split('.'):
+                try:
+                    data = data[key]
+                except TypeError:
+                    data = data[int(key)]
+        except (KeyError, TypeError, ValueError):
+            return not_found
+        else:
+            return data
+
+
+    @staticmethod
+    def missing(data, *args):
+        """Implements the missing operator for finding missing variables."""
+        not_found = object()
+        if args and isinstance(args[0], list):
+            args = args[0]
+        ret = []
+        for arg in args:
+            if get_var(data, arg, not_found) is not_found:
+                ret.append(arg)
+        return ret
+
+
+    @staticmethod
+    def missing_some(data, min_required, args):
+        """Implements the missing_some operator for finding missing variables."""
+        if min_required < 1:
+            return []
+        found = 0
+        not_found = object()
+        ret = []
+        for arg in args:
+            if get_var(data, arg, not_found) is not_found:
+                ret.append(arg)
+            else:
+                found += 1
+                if found >= min_required:
+                    return []
+        return ret
+
+
+    operations = {
+        "==": soft_equals,
+        "===": hard_equals,
+        "!=": lambda a, b: not soft_equals(a, b),
+        "!==": lambda a, b: not hard_equals(a, b),
+        ">": lambda a, b: less(b, a),
+        ">=": lambda a, b: less(b, a) or soft_equals(a, b),
+        "<": less,
+        "<=": less_or_equal,
+        "!": lambda a: not a,
+        "!!": bool,
+        "%": lambda a, b: a % b,
+        "and": lambda *args: functools.reduce(lambda total, arg: total and arg, args, True),
+        "or": lambda *args: functools.reduce(lambda total, arg: total or arg, args, False),
+        "?:": lambda a, b, c: b if a else c,
+        "if": if_,
+        "in": lambda a, b: a in b if "__contains__" in dir(b) else False,
+        "cat": lambda *args: "".join(str(arg) for arg in args),
+        "+": plus,
+        "*": lambda *args: functools.reduce(lambda total, arg: total * float(arg), args, 1),
+        "-": minus,
+        "/": lambda a, b=None: a if b is None else float(a) / float(b),
+        "min": lambda *args: min(args),
+        "max": lambda *args: max(args),
+        "merge": merge,
+        "count": lambda *args: sum(1 if a else 0 for a in args),
+    }
+
+
+    def __call__(self, data):
+        "Does the given data satisfy the expression?"
+        return self.apply(self.expression, data)
+        
+    def apply(self, expression, data):
+        """Executes the json-logic with given data."""
+        # You've recursed to a primitive, stop!
+        if expression is None or not isinstance(expression, dict):
+            return expression
+
+        data = data or {}
+
+        operator = list(expression)[0]
+        values = expression[operator]
+
+        # Easy syntax for unary operators, like {"var": "x"} instead of strict
+        # {"var": ["x"]}
+        if not isinstance(values, list) and not isinstance(values, tuple):
+            values = [values]
+
+        # Recursion!
+        values = [self.apply(val, data) for val in values]
+
+        if operator == 'var':
+            return self.get_var(data, *values)
+        if operator == 'missing':
+            return self.missing(data, *values)
+        if operator == 'missing_some':
+            return self.missing_some(data, *values)
+
+        try:
+            return self.operations[operator](*values)
+        except KeyError:
+            raise ValueError("Unrecognized operation %s" % operator)
 
 
 if __name__ == "__main__":
@@ -245,4 +455,17 @@ if __name__ == "__main__":
     with db:
         db["x"] = {"erty": "apa"}
     print(db, list(db))
+    for expression in [{"==": [{"var": "b"}, 2]},
+                  {"or": [
+                      {"and": [
+                          {"==": [{"var": "b"}, 2]},
+                          {"in": [2, {"var": "d"}]}
+                      ]
+                       },
+                      {"===": [{"var": "erty"}, "apa"]}
+                  ]
+                   }
+                  ]:
+        logic = Logic(expression)
+        print(expression, logic(db["b"]), logic(db["x"]))
     db.close()
